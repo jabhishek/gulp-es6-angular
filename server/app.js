f8d1db17c3cfa31dfe3a807aeaaca760679082e7
jabhishek <abhi2000@gmail.com>
2015-04-23 11:30:15 +0100

    Change directory structure

--- server/app.js
+++ server/app.js
@@ -1,10 +1,21 @@
 var express = require('express');
+var path = require("path");
+var morgan = require('morgan');
 var port = process.env.PORT || 9000;
-
-var appPath = __dirname;
+var rootPath = path.normalize(__dirname + '/..');
+var appPath;
 
 var app = express();
+
+if (app.get("env") === "development") {
+    app.use(morgan('dev'));
+    //app.use(require('connect-livereload')());
+    appPath = path.join(rootPath, 'client');
+}
+
 app.use(express.static(appPath));
+
+
 
 app.route('/*')
     .get(function (req, res) {
