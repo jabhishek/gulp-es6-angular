524ef639890f6632d6e1424947be59bd37842f03
jabhishek <abhi2000@gmail.com>
2015-04-23 15:20:02 +0100

    Added traceur options, with references

--- client/system.config.js
+++ client/system.config.js
@@ -1,3 +1,9 @@
 System.config({
-	baseURL: '/'
+	baseURL: '/',
+	/* References for traceur options */
+	/* https://github.com/google/traceur-compiler/wiki/Options-for-Compiling */
+	/* https://github.com/google/traceur-compiler/blob/master/src/Options.js */
+	traceurOptions: {
+		symbols: true
+	}
 });
