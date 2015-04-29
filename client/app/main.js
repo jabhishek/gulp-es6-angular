4ff7367c9006518bca0b8a4162b195b2a430865c
jabhishek <abhi2000@gmail.com>
2015-04-29 21:45:03 +0100

    Added other project dependencies

--- client/app/main.js
+++ client/app/main.js
@@ -1,9 +1,18 @@
 import angular from 'angular';
+import 'angular-animate';
+import 'angular-aria';
+import 'angular-touch';
+import 'angular-material';
+
 import homeModule from './home/index';
 import mainConfig from './mainConfig';
 
 export var mainModule = angular.module('mainModule',
 	[
+		'ngAnimate',
+		'ngTouch',
+		'ngAria',
+		'ngMaterial',
 		homeModule.name,
 		mainConfig.name
 	]);
